107c80d877f19a1a059847e3589f39b0ca075f5e
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-08-27 15:23:08 -0300

    Iniciando projeto

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace FlexiTools
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sidebar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            var selected = sidebar.SelectedItem as NavButton;
+
+            navframe.Navigate(selected.Navlink);
+
+        }
+    }
+}
+  
