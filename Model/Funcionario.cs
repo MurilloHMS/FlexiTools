17f59f3138eb886cfcb53644f237e369268ac018
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-08-28 17:11:05 -0300

    Feat: criado tela de configurações para incluir os funcionários

---
+++ Model/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace FlexiTools.Model
+{
+    class Funcionario
+    {
+        public string Nome {  get; set; }
+
+        public async Task<List<Funcionario>> GetFuncionarios(string filePath)
+        {
+
+            if (!File.Exists(filePath))
+            {
+                return new List<Funcionario>();
+            }
+
+            using(var reader = new StreamReader(filePath))
+            {
+                string json = await reader.ReadToEndAsync();
+                var funcionarios = JsonConvert.DeserializeObject<List<Funcionario>>(json);
+                return funcionarios ?? new List<Funcionario>();
+            }
+
+        }
+
+        public async Task SetFuncionarios(string filePath, List<Funcionario> funcionarios)
+        {
+            string json = JsonConvert.SerializeObject(funcionarios, Formatting.Indented);
+            using (var writer = new StreamWriter(filePath))
+            {
+                await writer.WriteLineAsync(json);
+            }
+        }
+    }
+}
---
+++ Pages/Configuracoes.xaml.cs
@@ -0,0 +1,47 @@
+﻿using FlexiTools.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlexiTools.Pages
+{
+    /// <summary>
+    /// Interação lógica para Configuracoes.xam
+    /// </summary>
+    public partial class Configuracoes : Page
+    {
+        public Configuracoes()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string file = "funcionarios.json";
+            var dados = dgvFuncionarios.ItemsSource as List<Funcionario>;
+            Funcionario func = new Funcionario();
+            func.SetFuncionarios(file, dados);
+        }
+
+        private async void Page_Initialized(object sender, EventArgs e)
+        {
+            string file = "funcionarios.json";
+            Funcionario func = new Funcionario();
+            var funcionarios = await func.GetFuncionarios(file);
+            dgvFuncionarios.ItemsSource = funcionarios;            
+        }
+    }
+}
