c21a963d540a6ec997fd855d6b0455c02feeceeb
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-08-29 12:41:08 -0300

    refactor: melhorias de código UploadDadosFuncionarios
    melhorado o código para atender os padrões MVVM

--- MVVM/Pages/UploadDadosFuncionarios.xaml.cs
+++ MVVM/Pages/UploadDadosFuncionarios.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows;
 using System.Windows.Controls;
 
 
-namespace FlexiTools.Pages
+namespace FlexiTools.MVVM.Pages
 {
     public partial class UploadDadosFuncionarios : Page
     {
@@ -12,43 +12,6 @@ namespace FlexiTools.Pages
         {
             InitializeComponent();
         }
-
-        private async void btnExcel_Click(object sender, RoutedEventArgs e)
-        {
-            var dados = await Cartao.ObterDadosDasPlanilhasAsync();
-            dgvCartoes.ItemsSource = dados;
-        }
-
-        private async void btnSalvar(object sender, RoutedEventArgs e)
-        {
-            var dados = dgvCartoes.ItemsSource as IEnumerable<Cartao>;
-
-            if (dados == null || !dados.Any())
-            {
-                MessageBox.Show("Não há dados para salvar.");
-                return;
-            }
-
-            var dialog = new SaveFileDialog
-            {
-                Filter = "Arquivos Excel (*.xlsx)|*.xlsx",
-                Title = "Escolha onde salvar o arquivo"
-            };
-
-            if (dialog.ShowDialog() == true)
-            {
-                string caminhoArquivo = dialog.FileName;
-
-                try
-                {
-                    await Cartao.SalvarDados(caminhoArquivo, dados);
-                    MessageBox.Show("Dados salvos com sucesso.");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Erro ao salvar dados: {ex.Message}");
-                }
-            }
-        }
+        
     }
 }
---
+++ MVVM/ViewModel/UploadDadosFuncionariosViewModel.cs
@@ -0,0 +1,73 @@
+﻿using FlexiTools.MVVM.Model;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FlexiTools.MVVM.ViewModel
+{
+    public class UploadDadosFuncionariosViewModel : ViewModelBase
+    {
+        private ObservableCollection<Cartao> _cartoes;
+
+        public ObservableCollection<Cartao> Cartoes
+        {
+            get => _cartoes;
+            set
+            {
+                _cartoes = value;
+                OnPropertyChanged(nameof(Cartoes));
+            }
+        }
+
+        public ICommand ImportExcelComamnd { get; }
+        public ICommand SaveCommand { get; }
+
+        public UploadDadosFuncionariosViewModel()
+        {
+            Cartoes = new ObservableCollection<Cartao>();
+            ImportExcelComamnd = new RelayCommand(async () => await ImportExcelData());
+            SaveCommand = new RelayCommand(async () => await SaveData(), CanSaveData);
+        }
+
+
+        private async Task ImportExcelData()
+        {
+            var dados = await Cartao.ObterDadosDasPlanilhasAsync();
+            Cartoes.Clear();
+            foreach(var cartao in dados)
+            {
+                Cartoes.Add(cartao);
+            }
+        }
+
+        private async Task SaveData()
+        {
+            var dialog = new SaveFileDialog
+            {
+                Filter = "Arquivos Excel (*.xlsx)|*.xlsx",
+                Title = "Escolha onde salvar o arquivo"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                string caminhoArquivo = dialog.FileName;
+
+                try
+                {
+                    await Cartao.SalvarDados(caminhoArquivo, Cartoes);
+                    MessageBox.Show("Dados salvos com sucesso.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao salvar dados: {ex.Message}");
+                }
+            }
+        }
+
+        private bool CanSaveData()
+        {
+            return Cartoes.Count > 0;
+        }
+    }
+}
