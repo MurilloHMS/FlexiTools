0e4cee78a88da1fa859bb4755dace6d0693a09b1
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-08-29 14:52:41 -0300

    feature: adicionado treeview com dados agrupados por funcionário e categoria

--- MVVM/ViewModel/UploadDadosFuncionariosViewModel.cs
+++ MVVM/ViewModel/UploadDadosFuncionariosViewModel.cs
@@ -1,7 +1,12 @@
 ﻿using FlexiTools.MVVM.Model;
 using Microsoft.Win32;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace FlexiTools.MVVM.ViewModel
@@ -9,6 +14,7 @@ namespace FlexiTools.MVVM.ViewModel
     public class UploadDadosFuncionariosViewModel : ViewModelBase
     {
         private ObservableCollection<Cartao> _cartoes;
+        private ICollectionView _cartoesGroupedView;
 
         public ObservableCollection<Cartao> Cartoes
         {
@@ -17,6 +23,17 @@ namespace FlexiTools.MVVM.ViewModel
             {
                 _cartoes = value;
                 OnPropertyChanged(nameof(Cartoes));
+                UpdateGroupedData();
+            }
+        }
+
+        public ICollectionView CartoesGroupedView
+        {
+            get => _cartoesGroupedView;
+            private set
+            {
+                _cartoesGroupedView = value;
+                OnPropertyChanged(nameof(CartoesGroupedView));
             }
         }
 
@@ -30,15 +47,36 @@ namespace FlexiTools.MVVM.ViewModel
             SaveCommand = new RelayCommand(async () => await SaveData(), CanSaveData);
         }
 
-
         private async Task ImportExcelData()
         {
             var dados = await Cartao.ObterDadosDasPlanilhasAsync();
             Cartoes.Clear();
-            foreach(var cartao in dados)
+            foreach (var cartao in dados)
             {
                 Cartoes.Add(cartao);
             }
+            UpdateGroupedData();
+        }
+
+        private void UpdateGroupedData()
+        {
+            var groupedData = Cartoes
+                .GroupBy(c => new { c.Nome })
+                .Select(g => new
+                {
+                    Nome = g.Key.Nome,
+                    Categorias = g
+                        .GroupBy(c => c.Categoria)
+                        .Select(cg => new
+                        {
+                            Categoria = cg.Key,
+                            ValorTotal = cg.Sum(c => c.Valor)
+                        })
+                })
+                .ToList();
+
+            CartoesGroupedView = new CollectionViewSource { Source = groupedData }.View;
+            CartoesGroupedView.Refresh();
         }
 
         private async Task SaveData()
