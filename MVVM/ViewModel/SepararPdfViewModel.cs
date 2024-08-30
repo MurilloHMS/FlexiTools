1d90327262c35df837c4b3d2e3eef758c39dd5e6
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-08-30 16:30:17 -0300

    Iniciado classe para separar PDF \

---
+++ MVVM/ViewModel/SepararPdfViewModel.cs
@@ -0,0 +1,50 @@
+﻿using FlexiTools.MVVM.Model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlexiTools.MVVM.ViewModel
+{
+    internal class SepararPdfViewModel : ViewModelBase
+    {
+        private ObservableCollection<SepararPDF> _pages;
+
+        public ObservableCollection<SepararPDF> Pages
+        {
+            get => _pages;
+            set
+            {
+                _pages = value;
+                OnPropertyChanged(nameof(Pages));
+            }
+        }
+
+        public ICommand OpenFiles {  get;}
+
+        public SepararPdfViewModel()
+        {
+            OpenFiles = new RelayCommand(async () => await LoadDataAsync());
+        }
+
+        private async Task LoadDataAsync()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() != true)
+            {
+                return;
+            }
+            Pages = new ObservableCollection<SepararPDF>(SepararPDF.GetSeparatePdfByPage(ofd.FileName, Path.GetDirectoryName(ofd.FileName)));
+        }
+
+        private async Task SaveFiles()
+        {
+            
+        }
+    }
+}
