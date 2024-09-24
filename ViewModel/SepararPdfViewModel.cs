95fa1af5de5820d5185974d295b28531d2d507b8
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-09-24 14:34:15 -0300

    Corrigido model SepararPDF

--- ViewModel/SepararPdfViewModel.cs
+++ ViewModel/SepararPdfViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using FlexiTools.Model;
 using Microsoft.Win32;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
+using System.IO;
 
 namespace FlexiTools.ViewModel
 {
@@ -41,7 +45,7 @@ namespace FlexiTools.ViewModel
 
             if (ofd.ShowDialog() != true) { return; }
             _pdfFileName = ofd.FileName;
-            Pages = new ObservableCollection<SepararPDF>(SepararPDF.GetSeparatePdfByPage(_pdfFileName));
+            Pages = new ObservableCollection<SepararPDF>(GetSeparatePdfByPage(_pdfFileName));
         }
 
         private async Task SaveFiles()
@@ -55,12 +59,70 @@ namespace FlexiTools.ViewModel
 
             if (ofd.ShowDialog() != true) { return; }
             _pdfDirectory = ofd.FolderName;
-            SepararPDF.SeparatedPdfByPage(_pdfFileName, _pdfDirectory, pages);
+            SeparatedPdfByPage(_pdfFileName, _pdfDirectory, pages);
         }
 
         private bool CanSave()
         {
             return Pages != null;
+        }
+
+        public static List<SepararPDF> GetSeparatePdfByPage(string inputPdfPath)
+        {
+            try
+            {
+                if (!File.Exists(inputPdfPath))
+                {
+                    MessageBox.Show("Arquivo PDF não encontrado.", "Error: File has not found", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return new List<SepararPDF>();
+                }
+
+                PdfDocument inputDocument = PdfReader.Open(inputPdfPath, PdfDocumentOpenMode.Import);
+                List<SepararPDF> archives = new List<SepararPDF>();
+                for (int i = 0; i < inputDocument.Pages.Count; i++)
+                {
+                    archives.Add(new SepararPDF { Nome = $"Pagina {i}" });
+                }
+
+                return archives ?? new List<SepararPDF>();
+            }
+            catch (Exception)
+            {
+                return new List<SepararPDF>();
+            }
+        }
+
+        public static void SeparatedPdfByPage(string inputPdfPath, string outputFolder, List<SepararPDF> lista)
+        {
+            if (!File.Exists(inputPdfPath))
+            {
+                MessageBox.Show("Arquivo PDF não encontrado.", "Error: File has not found", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                PdfDocument inputDocument = PdfReader.Open(inputPdfPath, PdfDocumentOpenMode.Import);
+
+                int pageNum = 0;
+
+                for (int pageNumber = 0; pageNumber < inputDocument.PageCount; pageNumber++)
+                {
+                    PdfDocument outputDocument = new PdfDocument();
+                    outputDocument.AddPage(inputDocument.Pages[pageNumber]);
+                    string outputFilePath = Path.Combine(outputFolder, $"{lista[pageNumber].Nome}.pdf");
+                    outputDocument.Save(outputFilePath);
+                    outputDocument.Close();
+                    pageNum++;
+                }
+
+                MessageBox.Show($"Arquivos Separados em {pageNum} arquivos e salvos em : \n\n {outputFolder}", "Salvamento de Arquivos", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                inputDocument.Close();
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
